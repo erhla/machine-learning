d2ddda8b70f92f219bc043839305151105d50349
Eric Langowski <langowski@cs.uchicago.edu>
2019-04-09 23:45:33 -0400

    working on HW1

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # machine-learning
+## Eric Langowski
 
-## HW 1
-Code is in HW1.py
-Analysis graphs in HW1.ipynb
+### HW 1 Submission Information
+Files: hw1.py, HW1.ipynb
 
-https://api.census.gov/data/2017/acs/acs5/subject?get=NAME,S0101_C01_001E,S1701_C02_001E,S1901_C01_012E,S1501_C02_015E,S2303_C02_033E,S1601_C02_003E&for=tract:*&in=state:17&in=county:031
+hw1.py holds helper functions which import data from APIs and
+geocode information.
+
+HW1.ipynb includes graphs, analysis, and the answers to given questions.
--- hw1.py
+++ hw1.py
@@ -2,9 +2,6 @@
 Homework 1
 Eric Langowski
 '''
-#remove API key
-
-
 import pandas as pd
 from sodapy import Socrata
 import requests
@@ -17,8 +14,7 @@ CRIMES_17 = 'd62x-nvdr'
 CENSUS_TRACTS = '74p9-q2aq'
 
 def get_crime_data(id):
-	client = Socrata('data.cityofchicago.org', 'HOfdnr49zcHGX5cuvxwbyTJjn',
-		username='langowski@uchicago.edu',password='GCpGLLQ3EmJiNDP')
+	client = Socrata('data.cityofchicago.org', None)
 	current = 0
 	results = client.get(id, limit=2000, offset=current)
 	while len(results) > 0:
@@ -30,8 +26,6 @@ def get_crime_data(id):
 			break
 		print(current)
 	return pd.DataFrame.from_records(results)
-
-
 
 def download_17_18():
 	crime_17 = get_crime_data(CRIMES_17)
@@ -50,14 +44,25 @@ def download_17_18_alt():
 	df['Date'] = pd.to_datetime(df['Date'])
 	return df
 
-col_ls = ["S0101_C01_001E","S1701_C02_001E","S1901_C01_012E","S1501_C02_015E","S2303_C02_033E","S1601_C02_003E"]
+census_variables = {"S0101_C01_001E": "Population",
+					"S1701_C02_001E": "Population in Poverty",
+					"S1901_C01_012E": "Median income",
+					"S1501_C02_015E": "Percent Population with Bachelors",
+					"S2303_C02_033E": "Percent Population working full-time",
+					"S1601_C02_003E": "Percent speaking not English"}
+
 census_url = "https://api.census.gov/data/2017/acs/acs5/subject?get="
 census_frag = "&for=tract:*&in=state:17&in=county:031"
 
+def get_census_data():
+	url = census_url + ",".join(census_variables.keys()) + census_frag
+	df = pd.read_json(url, orient='records')
+	new_cols = list(census_variables.values()) + ['state', 'county', 'tract']
+	df.columns = new_cols
+	df = df.drop(df.index[0])
+	return df
 
-
-
-def geomerge(census_data, crime_data):
+def geomerge(crime_data):
 	#Some of this code is from my group's 122 project
 	#which made a spatial join on Chicago's neighborhoods
 
@@ -68,21 +73,13 @@ def geomerge(census_data, crime_data):
 	df = geopandas.GeoDataFrame(df, geometry='the_geom')
 	df.crs = {'init': 'espg:4326'}
 	
-	crime_data['coordinates'] = list(zip(df[lon], df[lat]))
-	crime_data.loc[:, 'coordinates'] = crime_data['coordinates'].apply(shapely.geometry.Point)
-	geodf = geopandas.GeoDataFrame(crime_data, geometry='coordinates')
+	crime_data = crime_data[crime_data['Longitude'].notna()]
+	crime_data['Location'] = crime_data['Location'].str.strip('()').str.split(',')    
+	crime_data['Location'] = crime_data['Location'].apply(lambda x: (float(x[1]), float(x[0])))#tuple(map(float,x)))     
+	crime_data.loc[:, 'Location'] = crime_data['Location'].apply(shapely.geometry.Point)
+	geodf = geopandas.GeoDataFrame(crime_data, geometry='Location')
 	geodf.crs = {'init': 'espg:4326'}
+	#geodf = geodf.to_crs({'init': 'espg:4326'})
 
-	merged = geopandas.sjoin(crime_data, df, how='left', op='within')
+	return geopandas.sjoin(geodf, df, how='left', op='within')
 
-
-	return df
-
-
-
-def get_census_data():
-	url = census_url + ",".join(col_ls) + census_frag
-	df = pd.read_json(url, orient='records')
-	df.columns = df.iloc[0]
-	df = df.drop(df.index[0])
-	return df
